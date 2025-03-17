22216f65f5e4cf31395da0d4abd8377f371263e3
shithu-swivel <shithu@swivelgroup.com.au>
2025-03-17 15:49:43 +0530

    Update util.js

--- utils/util.js
+++ utils/util.js
@@ -38,7 +38,7 @@ export function getCapabilities() {
         {
           browserName: "chrome",
           "goog:chromeOptions": {
-            args: ["start-maximized", "--allow-file-access-from-files", "disable-gpu", "--no-sandbox"],
+            args: ["start-maximized", "--allow-file-access-from-files", "disable-gpu", "--no-sandbox", "--user-data-dir=false"],
           },
           ...commonBrowserConfig, // Spread the common browser config object
         },
