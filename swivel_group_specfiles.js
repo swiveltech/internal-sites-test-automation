726eccfcf7675dc39b913621b7e4754de0a775b7
Dilshan Kahawita <dilshan@swivelgroup.com.au>
2025-03-19 12:51:09 +0530

    uncommitted test case lines

--- swivel_group_specfiles.js
+++ swivel_group_specfiles.js
@@ -6,14 +6,14 @@ const specConfig = [
   {
     specs: [
       // This is Swivel Group Test cases
-      // "../test/scripts/SwivelGroup/tc_Home.js",
-      // "../test/scripts/SwivelGroup/tc_AboutUs.js",
-      // "../test/scripts/SwivelGroup/tc_Careers.js",
-      // "../test/scripts/SwivelGroup/tc_ContactUs.js",
-      // "../test/scripts/SwivelGroup/tc_Cookie.js",
-      // "../test/scripts/SwivelGroup/tc_Newsroom.js",
+      "../test/scripts/SwivelGroup/tc_Home.js",
+      "../test/scripts/SwivelGroup/tc_AboutUs.js",
+      "../test/scripts/SwivelGroup/tc_Careers.js",
+      "../test/scripts/SwivelGroup/tc_ContactUs.js",
+      "../test/scripts/SwivelGroup/tc_Cookie.js",
+      "../test/scripts/SwivelGroup/tc_Newsroom.js",
       "../test/scripts/SwivelGroup/tc_PrivacyPolicy.js",
-      // "../test/scripts/SwivelGroup/tc_Sitemap.js",
+      "../test/scripts/SwivelGroup/tc_Sitemap.js",
     ],
     testingPlan: "smoke",
     maxInstances: 1,
