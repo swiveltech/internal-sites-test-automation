ea94688d13507d7c08fe2450120b302b1ff1ec00
Dilshan Kahawita <dilshan@swivelgroup.com.au>
2025-04-04 11:15:27 +0530

    for the R&D screen size issue check

---
+++ screensize.js
@@ -0,0 +1,15 @@
+/**
+ * Add relevant spec file path here which you need to test
+ */
+
+const specConfig = [
+  {
+    specs: [
+      // This is Swivel Tech Test cases
+      "../test/scripts/SwivelTech/tc_ScreenSize.js",
+    ],
+    testingPlan: "smoke",
+    maxInstances: 1,
+  },
+];
+export default specConfig;
---
+++ test/scripts/SwivelTech/tc_ScreenSize.js
@@ -0,0 +1,19 @@
+import config from "../../../config.js";
+import LIB_Common from "../../components/LIB_Common.js";
+import Data_Home from "../../data/SwivelTech/dt_home.js";
+import Data_AboutUs from "../../data/SwivelTech/dt_aboutUs.js";
+import Data_HowWeWork from "../../data/SwivelTech/dt_howWeWork.js";
+import Data_Resources from "../../data/SwivelTech/dt_resources.js";
+import LIB_Home from "../../components/SwivelTech/LIB_Home.js";
+import Data_OurServices from "../../data/SwivelTech/dt_ourServices.js";
+
+describe("Swivel Tech Site -> Home Page", () => {
+  it("Verify the Home Page Screen size", async () => {
+    await LIB_Common.bc_StartAllureReportStep(
+      "Swivel Tech -> Home Page Verification",
+    );
+    await LIB_Common.bc_OpenApplication(config.URLS.SWIVEL_TECH);
+    await LIB_Common.bc_ClickOnButton("Accept", 1);
+    await LIB_Common.bc_TakeScreenShot("Screen Size");
+  });
+});
